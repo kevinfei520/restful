d7f030f3c71cd1c84b349e9a3b168b8294f9071d
jingfeiMac <794783766@qq.com>
2019-08-13 16:24:38 +0800

    sdasdasjd

--- application/route.php
+++ application/route.php
@@ -10,11 +10,12 @@
 // +----------------------------------------------------------------------
 use think\Route;
 
+Route::miss('Error/index');
+Route::rule(':version/token/token','api/:version.Token/token');  //token验证
+Route::resource(':version/goods','api/:version.Goods');//TEST
 Route::resource(':version/user','api/:version.User');   //注册一个资源路由，对应restful各个方法
 Route::resource(':version/course','api/:version.Course');   //注册一个资源路由，对应restful各个方法
 Route::resource(':version/classroom','api/:version.Classroom');   //注册一个资源路由，对应restful各个方法
-Route::rule(':version/token/token','api/:version.Token/token');		
-Route::miss('Error/index');
 
 return [
     '__pattern__' => [
